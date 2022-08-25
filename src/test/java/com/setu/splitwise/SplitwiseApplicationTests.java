aeef0e24f785b7022f6358b4b3f21017cdd79794
R.Maurya <rishikesh.maurya@myntra.com>
2022-08-25 19:38:37 +0530

    first commit

---
+++ src/test/java/com/setu/splitwise/SplitwiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.setu.splitwise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitwiseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
